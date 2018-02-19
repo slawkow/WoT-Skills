69505def4cf2d904e5adb7f28f139600c4ad1db3
slawkow <szczurek.slawek@gmail.com>
2018-02-19 20:30:41 +0100

    deleted redundant beans declaration

--- src/main/java/pl/slawek/wotskills/WebConfig.java
+++ src/main/java/pl/slawek/wotskills/WebConfig.java
@@ -1,25 +1,12 @@
 package pl.slawek.wotskills;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import pl.slawek.wotskills.service.PlayerService;
-import pl.slawek.wotskills.service.VehiclesService;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Bean
-    public VehiclesService vehiclesService() {
-        return new VehiclesService();
-    }
-
-    @Bean
-    public PlayerService playerService() {
-        return new PlayerService();
-    }
 }
